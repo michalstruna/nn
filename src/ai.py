5963eebc9c416329a9f5755b979215eb4153aa9a
Michal Struna <michal.l.struna@gmail.com>
2024-06-14 22:17:40 +0200

    optimize perceptron

--- src/ai.py
+++ src/ai.py
@@ -2,49 +2,73 @@ import numpy as np
 
 class Perceptron:
 
-	def __init__(self, input_shape):
-		self.input_shape = input_shape
-		self.weights = np.random.rand(input_shape[0])
-		self.bias = np.random.rand(1)
+	def __init__(self):
+		self.weights = None
+		self.boundaries = None
+	
+	def predict(self, input):
+		normalized = self._normalize(input)
+		z = self._linear(normalized)
+		return self._activation(z)
+	
+	def _split_inputs(self, inputs, targets, test_coeff):
+		split_index = round(inputs.shape[0] * (1 - test_coeff))
+		train_inputs, test_inputs = inputs[:split_index], inputs[split_index:]
+		train_targets, test_targets = targets[:split_index], targets[split_index:]
+		return train_inputs, train_targets, test_inputs, test_targets
 
-	def train(self, inputs, expected, epochs=100, rate=0.05, test=0.25):
+
+	def fit(self, inputs, targets, epochs=100, rate=0.01, test=0.25):
+		_, *input_shape = inputs.shape
+		self._create_boundaries(inputs)
+
+		if self.weights is None:
+			self.weights = np.random.rand(input_shape[0] + 1)
+
+		train_inputs, train_targets, test_inputs, test_targets = self._split_inputs(inputs, targets, test)
 		error_history = []
-		split_index = round(inputs.shape[0] * test)
-
-		train_inputs = inputs[:split_index]
-		test_inputs = inputs[split_index:]
-
-		train_expected = expected[:split_index]
-		test_expected = expected[split_index:]
-
+		
 		for _ in range(epochs):
 			epoch_error_history = []
 
 			for i in range(len(train_inputs)):
-				weighted_sum = self._sum(train_inputs[i], self.weights)
-				result = self._activation(weighted_sum)
-				error = np.sum(result - train_expected[i])
-				self.weights -= error * rate
-				self.bias -= error * rate
+				error = self._train(train_inputs[i], train_targets[i], rate)
 				epoch_error_history.append(np.abs(error))
 
 			error_history.append(np.mean(epoch_error_history))
 
-		results = test_expected.copy()
-
-		for i in range(len(test_inputs)):
-			weighted_sum = self._sum(test_inputs[i], self.weights)
-			results[i][0] = self._activation(weighted_sum)
-
 		return {
-			"results": results,
-			"expected": test_expected,
-			"error_history": error_history,
-			"test_error": np.sum(np.abs(test_expected - results))
+			"error_history": error_history
 		}
+	
+	def _train(self, input, target, rate):
+		prediction = self.predict(input)
+		error = self._loss(prediction, target)
+		self.weights[1:] -= rate * error * input
+		self.weights[0] -= rate * error
+		return error
 
-	def _sum(self, x, w):
-		return np.sum(x * w) - self.bias
+	def _loss(self, prediction, target):
+		return prediction - target
+	
+	def _linear(self, input):
+		return input @ self.weights[1:].T + + self.weights[0]
+	
+	def _activation(self, z):
+		return 1 if z >= 0 else 0
+	
+	def _create_boundaries(self, inputs):
+		self.boundaries = []
 
-	def _activation(self, x):
-		return 1 if x > 0 else 0
+		for i in range(inputs.shape[1]):
+			minimum, maximum = inputs[:,i].min(), inputs[:,i].max()
+			self.boundaries.append((minimum, maximum))
+	
+	def _normalize(self, input):
+		result = input.copy()
+
+		for i in range(len(self.boundaries)):
+			minimum, maximum = self.boundaries[i]
+			result[i] = (input[i] - minimum) / (maximum - minimum)
+
+		return result
--- src/main.py
+++ src/main.py
@@ -5,20 +5,21 @@ from random import randrange
 
 data = []
 
-for i in range(100):
-	x1 = randrange(140, 210)
-	y = 1 if x1 < 170 else 0
-	data.append([x1, y])
+for i in range(1000):
+	x1 = randrange(140, 220)
+	x2 = randrange(40, 110)
+	y = 1 if ((x1 + x2) < 250) else 0
+	data.append([x1, x2, y])
 
-data = np.array(data)
+data = np.array(data, dtype=float)
 
 inputs = data[:, 0:data.shape[1] - 1]
 expected = data[:, data.shape[1] - 1:]
-_, *input_shape = inputs.shape
 
 
-nn = Perceptron(input_shape=input_shape)
-training = nn.train(inputs=inputs,expected=expected)
+
+nn = Perceptron()
+training = nn.fit(inputs, expected)
 
 print(training["error_history"])
 
